9852d5252443b862d4082770ea0b336ef2568921
jongho143 <sumaoo20@naver.com>
2020-08-27 01:28:20 +0900

    숙제 진행중

--- 0823.js
+++ 0823.js
@@ -6,6 +6,75 @@
 // 6. 숫자 넣는 input, 제곱 버튼 만들고 버튼 누르면 숫자의 제곱값 출력하기
 // 7. Div, 버튼 만들고 버튼 누르면 Div 투명도(opacity) 1 부터 0까지 만들기, 시간은 3초동안 (애니메이션)
 
+//1번 문제 복습
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//2번 문제 복습
+// for (let i = 10; i >= 1; i--) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+//3번 문제 복습
+// const PizzaArray = [
+//   {
+//     name: "포테이토",
+//     price: "100",
+//   },
+//   {
+//     name: "불고기",
+//     price: "1000",
+//   },
+//   {
+//     name: "그냥",
+//     price: "1002",
+//   },
+//   {
+//     name: "페퍼로니",
+//     price: "1030",
+//   },
+//   {
+//     name: "고구마",
+//     price: "1020",
+//   },
+//   {
+//     name: "소시지",
+//     price: "1100",
+//   },
+//   {
+//     name: "단호박",
+//     price: "1400",
+//   },
+//   {
+//     name: "파인애플",
+//     price: "5100",
+//   },
+//   {
+//     name: "애플",
+//     price: "9100",
+//   },
+// ];
+
+//4번 문제 복습
+// for (let i = 0; i < PizzaArray.length; i++) {
+//   const pizza = PizzaArray[i];
+//   const menu = `${pizza.name} 피자는 ${pizza.price} 원 입니다.`;
+//   console.log(menu);
+// }
+
+//5번 문제 복습
+function powerOfTwo(num) {
+  const power = num * num;
+  return power;
+}
+const result = powerOfTwo(3);
+console.log(result);
+
 // 1번 문제
 // for (let i = 1; i <= 10; i++) {
 //   if (i % 2 === 0) {
@@ -13,8 +82,8 @@
 //   }
 // }
 
-// for (let index = 1; index <= 10; index++) {
-//   console.log(index);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
 // }
 
 // 2번 문제
--- HomeWork.js
+++ HomeWork.js
@@ -3,3 +3,18 @@
 // 3. 함수에 숫자, 제곱할 수 넣고 제곱한 결과 값 반환하기 power(num, 제곱할 수) 제곲 값 반환
 // 4. 노트북 오브젝트 배열 만들고 name노트북은 price원 입니다. 출력하는데 price 1000원 이상일 때만 출력
 // 5. 함수에 숫자 넣으면 1 ~ 10 까지 증가 값 더하기 ex) 1 넣으면 num + 1 + 2 + 3 + 4... + 10
+
+//1번 문제
+// function number() {
+
+// }
+
+//2번 문제
+
+//3번 문제
+// function powerOfTwo(num) {
+//     const power = num * num;
+//     return power;
+//   }
+//   const result = powerOfTwo(3);
+//   console.log(result);
